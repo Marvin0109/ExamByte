18873f13ea2540d50e37f01f395b258bf520ae62
muz70wuc <Denis.Strakhov@hhu.de>
2024-11-08 02:12:40 +0100

    ContactTest pfad wurde korrigiert und ein test ergänzt. warum gradle build die tests nicht bauen kann ist weiterhin ein rätsel :(

--- src/test/java/exambyte/main/controllers/ContactTest.java
+++ src/test/java/exambyte/main/controllers/ContactTest.java
@@ -1,3 +1,5 @@
+package exambyte.main.controllers;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,10 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest
+@WebMvcTest(ContactController.class)
 public class ContactTest {
 
     @Autowired
@@ -18,6 +21,14 @@ public class ContactTest {
     @DisplayName("Die Kontaktseite ist erreichbar für GET-Requests")
     void test_01() throws Exception {
         mvc.perform(get("/contact"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(view().name("contact"));
+    }
+
+    @Test
+    @DisplayName("Der Kontakt-Endpunkt verarbeitet unerwartete Methoden korrekt")
+    void test_02() throws Exception {
+        mvc.perform(post("/contact"))
+                .andExpect(status().isMethodNotAllowed());
     }
 }
