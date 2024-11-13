d3803eeaa97abc5d1359550ad67fc58e350ef21d
Marvin Wendeborn <marvin.wendeborn@gmx.de>
2024-11-13 16:31:23 +0100

    Navigations-Logik von HTML-Pages getrennt, dasselbe gilt für js

--- src/main/java/exambyte/main/controllers/ContactController.java
+++ src/main/java/exambyte/main/controllers/ContactController.java
@@ -1,13 +1,16 @@
 package exambyte.main.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactController {
 
     @GetMapping("/contact")
-    public String contact() {
+    public String contact(Model model, HttpServletRequest request) {
+        model.addAttribute("currentPath", request.getRequestURI());
         return "contact";
     }
 }
--- src/main/java/exambyte/main/controllers/WebController.java
+++ src/main/java/exambyte/main/controllers/WebController.java
@@ -1,13 +1,16 @@
 package exambyte.main.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class WebController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, HttpServletRequest request) {
+        model.addAttribute("currentPath", request.getRequestURI());
         return "index";
     }
 }
