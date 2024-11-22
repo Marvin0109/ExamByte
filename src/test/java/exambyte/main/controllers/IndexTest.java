eff103572772f01136bdb47152763fa23201de92
Marvin Wendeborn <marvin.wendeborn@gmx.de>
2024-11-22 15:34:29 +0100

    Tests geschrieben und überarbeitet

--- src/test/java/exambyte/main/controllers/IndexTest.java
+++ src/test/java/exambyte/main/controllers/IndexTest.java
@@ -1,26 +1,28 @@
 package exambyte.main.controllers;
 
+import exambyte.main.config.MethodSecurityConfig;
+import exambyte.main.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(WebController.class)
+@Import({SecurityConfig.class, MethodSecurityConfig.class})
 public class IndexTest {
 
     @Autowired
     MockMvc mvc;
 
     @Test
-    @DisplayName("GET-Request auf die Startseite")
+    @DisplayName("Startseite ist öffentlich erreichbar")
     void test_01() throws Exception {
         mvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));;
+                .andExpect(status().isOk());
     }
 }
