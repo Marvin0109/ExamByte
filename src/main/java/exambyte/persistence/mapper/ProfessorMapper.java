d068d54048d5ac2bd709d693ae8698acaa3262c2
muz70wuc <Denis.Strakhov@hhu.de>
2025-01-23 15:30:32 +0100

    [FEATURE] toEntity ProfessorMapper

--- src/main/java/exambyte/persistence/mapper/ProfessorMapper.java
+++ src/main/java/exambyte/persistence/mapper/ProfessorMapper.java
@@ -2,10 +2,14 @@ package exambyte.persistence.mapper;
 
 import exambyte.domain.aggregate.user.Professor;
 import exambyte.persistence.entities.ProfessorEntity;
+import org.springframework.security.core.parameters.P;
 
 public class ProfessorMapper {
 
     public static Professor toDomain(ProfessorEntity professorEntity) {
         return Professor.of(professorEntity.getId(), professorEntity.getName());
+    }
+    public static ProfessorEntity toEntity(Professor professor) {
+        return new ProfessorEntity(professor.getId(), professor.getName());
     }
 }
