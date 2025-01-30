fe254fff4d8fe8b4e83bd97fb5296a4514b89114
Marvin0109 <marvin.wendeborn@gmx.de>
2025-01-30 15:17:55 +0100

    [Test] ArchUnit Test For AggregateRoot

---
+++ src/test/java/exambyte/domain/AggregateRootTest.java
@@ -0,0 +1,26 @@
+package exambyte.domain;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.axonframework.modelling.command.AggregateRoot;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+public class AggregateRootTest {
+
+    private final JavaClasses importedClasses = new ClassFileImporter()
+            .importPackages("exambyte.domain.aggregate.exam");
+
+    @Test
+    @DisplayName("AggregatRoot Annotation Test")
+    void testAggregateRoot() {
+        ArchRule rule = classes()
+                .that().haveSimpleNameContaining("Test")
+                .should().beAnnotatedWith(AggregateRoot.class);
+
+        rule.check(importedClasses);
+    }
+}
