351719b3d6a9b92af2be09441d8548cf8ca71f8a
Marvin0109 <marvin.wendeborn@gmx.de>
2025-02-09 16:04:42 +0100

    [Refactor] Dead Code Removed

--- src/main/java/exambyte/web/controllers/GlobalExceptionHandler.java
+++ src/main/java/exambyte/web/controllers/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package exambyte.web.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.bind.annotation.ResponseStatus;
