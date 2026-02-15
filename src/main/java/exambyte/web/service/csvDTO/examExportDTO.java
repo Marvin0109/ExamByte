329d716ebd7ce50e7575728c52aae476f5d1cdc1
Marvin0109 <marvin.wendeborn@gmx.de>
2026-02-15 13:44:48 +0100

    Add: Export DTOs

---
+++ src/main/java/exambyte/web/service/csvDTO/examExportDTO.java
@@ -0,0 +1,12 @@
+package exambyte.web.service.csvDTO;
+
+public class examExportDTO {
+
+    private String examTitle;
+    private String author;
+    private String frageText;
+    private int punkte;
+    private String frageTyp;
+    private String antwortMoeglichkeiten;
+    private String loesungen;
+}
---
+++ src/main/java/exambyte/web/service/csvDTO/reviewExportDTO.java
@@ -0,0 +1,14 @@
+package exambyte.web.service.csvDTO;
+
+public class reviewExportDTO {
+
+    private String examTitle;
+    private String author;
+    private String frageText;
+    private int maxPunkte;
+    private String frageTyp;
+    private String antwortMoeglichkeiten;
+    private String studiAntworten;
+    private String bewertung;
+    private double erreichtePunkte;
+}
