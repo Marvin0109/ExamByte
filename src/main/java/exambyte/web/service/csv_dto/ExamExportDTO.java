fe359feee0de40ce39b544e93a39f6d638cb3c0e
Marvin0109 <marvin.wendeborn@gmx.de>
2026-02-15 13:56:27 +0100

    Getter and setter for export dto

--- src/main/java/exambyte/web/service/csv_dto/ExamExportDTO.java
+++ src/main/java/exambyte/web/service/csv_dto/ExamExportDTO.java
@@ -9,4 +9,60 @@ public class ExamExportDTO {
     private String frageTyp;
     private String antwortMoeglichkeiten;
     private String loesungen;
+
+    public String getExamTitle() {
+        return examTitle;
+    }
+
+    public void setExamTitle(String examTitle) {
+        this.examTitle = examTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getFrageText() {
+        return frageText;
+    }
+
+    public void setFrageText(String frageText) {
+        this.frageText = frageText;
+    }
+
+    public int getPunkte() {
+        return punkte;
+    }
+
+    public void setPunkte(int punkte) {
+        this.punkte = punkte;
+    }
+
+    public String getFrageTyp() {
+        return frageTyp;
+    }
+
+    public void setFrageTyp(String frageTyp) {
+        this.frageTyp = frageTyp;
+    }
+
+    public String getAntwortMoeglichkeiten() {
+        return antwortMoeglichkeiten;
+    }
+
+    public void setAntwortMoeglichkeiten(String antwortMoeglichkeiten) {
+        this.antwortMoeglichkeiten = antwortMoeglichkeiten;
+    }
+
+    public String getLoesungen() {
+        return loesungen;
+    }
+
+    public void setLoesungen(String loesungen) {
+        this.loesungen = loesungen;
+    }
 }
--- src/main/java/exambyte/web/service/csv_dto/ReviewExportDTO.java
+++ src/main/java/exambyte/web/service/csv_dto/ReviewExportDTO.java
@@ -11,4 +11,76 @@ public class ReviewExportDTO {
     private String studiAntworten;
     private String bewertung;
     private double erreichtePunkte;
+
+    public String getExamTitle() {
+        return examTitle;
+    }
+
+    public void setExamTitle(String examTitle) {
+        this.examTitle = examTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getFrageText() {
+        return frageText;
+    }
+
+    public void setFrageText(String frageText) {
+        this.frageText = frageText;
+    }
+
+    public int getMaxPunkte() {
+        return maxPunkte;
+    }
+
+    public void setMaxPunkte(int maxPunkte) {
+        this.maxPunkte = maxPunkte;
+    }
+
+    public String getFrageTyp() {
+        return frageTyp;
+    }
+
+    public void setFrageTyp(String frageTyp) {
+        this.frageTyp = frageTyp;
+    }
+
+    public String getAntwortMoeglichkeiten() {
+        return antwortMoeglichkeiten;
+    }
+
+    public void setAntwortMoeglichkeiten(String antwortMoeglichkeiten) {
+        this.antwortMoeglichkeiten = antwortMoeglichkeiten;
+    }
+
+    public String getStudiAntworten() {
+        return studiAntworten;
+    }
+
+    public void setStudiAntworten(String studiAntworten) {
+        this.studiAntworten = studiAntworten;
+    }
+
+    public String getBewertung() {
+        return bewertung;
+    }
+
+    public void setBewertung(String bewertung) {
+        this.bewertung = bewertung;
+    }
+
+    public double getErreichtePunkte() {
+        return erreichtePunkte;
+    }
+
+    public void setErreichtePunkte(double erreichtePunkte) {
+        this.erreichtePunkte = erreichtePunkte;
+    }
 }
